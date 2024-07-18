bb63fcaa039b0e96a20a4b3e6e54b58c2dd6120d
Sindhu0711 <138217021+Sindhu0711@users.noreply.github.com>
2024-07-19 02:21:50 +0530

    uploaded

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2024 MADE EASY. All rights reserved.</p>
+      
+    </div>
+  )
+}
+
+export default Footer
